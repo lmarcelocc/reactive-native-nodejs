080cfabb0b4006956e1fce13b44d767879731295
Marcelo Carvalho <marcelo.carvalho@cleverti.com>
2023-11-10 16:11:44 +0000

    🎉

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import nodejs from 'nodejs-mobile-react-native';
 
 export default function App() {
+  constructor()
+  {
+    nodejs.start("main.js");
+    nodejs.channel.addListener(
+      "message",
+      (msg) => {
+        alert("From node: " + msg);
+      },
+      this
+    );
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Message Node"
+        onPress={() => nodejs.channel.send('A message!')}
+      />
       <StatusBar style="auto" />
     </View>
   );
